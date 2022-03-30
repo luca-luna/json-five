f4d4c88d0dce41d6f8ad2453ec11bf0fe1fe8153
Maeve <maeveros@buffalo.edu>
2022-03-30 15:24:59 -0400

    wrote functions that create and list database entries for messages

--- TCPServer.py
+++ TCPServer.py
@@ -1,5 +1,6 @@
 import socketserver
 import response
+import database
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
@@ -30,6 +31,30 @@ if __name__ == "__main__":
     print("forbidden", response.forbidden(), flush=True)
     print("ok", response.ok("text/plain; charset=utf-8", "LOL".encode()), flush=True)
     print("file", response.file("front_end/index.html"), flush=True)
+    '''
+
+    '''
+    DB TESTS
+    homepage_message_info = {"message": "hi friends", "username": "Rob"}
+    print("create homepage message", database.addHomepageMessage(homepage_message_info), flush=True)
+    homepage_message_info2 = {"message": "hello", "username": "Sam"}
+    print("create homepage message 2", database.addHomepageMessage(homepage_message_info2), flush=True)
+    homepage_messages = database.listHomepageMessages()
+    print("homepage messages", homepage_messages, flush=True)
+    dm_info = {"message": "hi dude", "username1": "Debbie", "username2": "Rob"}
+    print("create dm", database.addDM(dm_info), flush=True)
+    dm_info2 = {"message": "yo", "username1": "Rob", "username2": "Debbie"}
+    print("create dm2", database.addDM(dm_info2), flush=True)
+    dm_info3 = {"message": "what is your favorite color?", "username1": "Sam", "username2": "Emily"}
+    print("create dm3", database.addDM(dm_info3), flush=True)
+    dm_info4 = {"message": "red", "username1": "Emily", "username2": "Sam"}
+    print("create dm4", database.addDM(dm_info4), flush=True)
+    dm_info5 = {"message": "cool, mine is blue", "username1": "Sam", "username2": "Emily"}
+    print("create dm5", database.addDM(dm_info5), flush=True)
+    dm_messages = database.listDMs("Debbie", "Rob")
+    print("dm messages", dm_messages, flush=True)
+    dm_messages2 = database.listDMs("Emily", "Sam")
+    print("dm messages2", dm_messages2, flush=True)
     '''
 
     # Create the server, binding to localhost on port 9999
--- database.py
+++ database.py
@@ -4,15 +4,94 @@ mongo_client = MongoClient("mongo")
 db = mongo_client['312-Project']
 
 user_collection = db['users']
-user_id_collection = db['next_id']
+user_id_collection = db['next_id_users']
+
+message_collection = db['messages']
+message_id_collection = db['next_id_messages']
+
+dm_collection = db['dms']
+dm_id_collection = db['next_id_dms']
 
 
-def create_user_id():
-    id = user_id_collection.find_one({})
+'''
+message_collection is for the homepage messaging area
+this just stores the messages and the usernames associated w them
+
+dm_collection is for the dms
+this stores the messages and the usernames of both participants
+'''
+
+
+def create_id(collection):
+    id = collection.find_one({})
     if id:
         next_id = int(id['prev_id']) + 1
-        user_id_collection.update_one({}, {'$set': {'prev_id': next_id}})
+        collection.update_one({}, {'$set': {'prev_id': next_id}})
         return next_id
     else:
-        user_id_collection.insert_one({'prev_id': 1})
+        collection.insert_one({'prev_id': 1})
         return 1
+
+
+
+def addHomepageMessage(info):
+
+    id = create_id(message_id_collection)
+
+    info["id"] = id
+
+    message_collection.insert_one(info)
+
+    created_record = message_collection.find_one({"id": id})
+    del created_record["_id"]
+
+    return created_record
+
+
+
+def addDM(info):
+
+    id = create_id(dm_id_collection)
+
+    info["id"] = id
+
+    dm_collection.insert_one(info)
+
+    created_record = dm_collection.find_one({"id": id})
+    del created_record["_id"]
+
+    return created_record
+
+
+
+def listHomepageMessages():
+
+    docs = []
+    for doc in message_collection.find({}):
+        del doc["_id"]
+        docs.append(doc)
+
+    return docs
+
+
+
+def listDMs(username1, username2):
+
+    docs = []
+    for doc in dm_collection.find( { "$or": [ { "username1": username1, "username2": username2}, {"username1": username2, "username2": username1} ] }):
+        del doc["_id"]
+        docs.append(doc)
+
+    return docs
+
+
+
+
+
+
+
+
+
+
+
+

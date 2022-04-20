e5814bb57ae8a70310e2bd045af732efddbf5f52
Maeve <maeveros@buffalo.edu>
2022-04-20 07:30:00 -0400

    Recreated WSFrame class

--- websocket.py
+++ websocket.py
@@ -16,6 +16,67 @@ def handshake_testing():
     
     accept = compute_accept(key)
     assert accept == expected, accept + "is not the same as" + expected
+
+
+
+
+class WSFrame:
+
+    def __init__(self, frame_bytes):
+
+        self.frame_bytes = frame_bytes
+        self.fin = 1
+        self.opcode = 0
+        self.maskbit = 0
+
+        self.payload_length = 27
+        self.payload = b"12345678"
+        self.first_mask_or_payload_byte = 0
+        self.parse_headers()
+        self.parse_payload_length()
+        self.finished_buffering = False
+        self.check_payload()
+
+
+
+    def parse_header(self):
+
+        self.opcode = self.frame_bytes[0] & 31
+        self.fin = self.frame_bytes[0] >> 7
+        self.maskbit = self.frame_bytes[1] >> 7
+
+
+    def parse_payload_length(self):
+
+        first_try = self.frame_bytes[1] & 127
+
+        if first_try < 126:
+            self.payload_length = first_try
+            self.first_mask_or_payload_byte = 2
+
+        elif first_try == 126:
+            payload_length = int(self.frame_bytes[2])
+            payload_length = payload_length << 8
+            payload_length = payload_length + self.frame_bytes[3]
+            self.payload_length = payload_length
+            self.first_mask_or_payload_byte = 4
+
+        elif first_try == 127:
+            payload_length = int(self.frame_bytes[2])
+            for i in range(3, 10):
+                payload_length = payload_length << 8
+                payload_length = payload_length + self.frame_bytes[i]
+            self.payload_length = payload_length
+            self.first_mask_or_payload_byte = 10
+
+        else:
+            print("Parsing Error: Length")
+
+
+
+    def check_payload(self):
+        return 
+
     
 # def test_parsing_frame():
 #     print()
